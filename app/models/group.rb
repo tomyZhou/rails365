5fa005f4a2da76598450b7a5e901c705f026b233
suifeng <ganweiliang886@gmail.com>
2015-11-17 20:48:43 +0800

    分类添加friendly_id

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,16 @@
+require "babosa"
 class Group < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders, :history]
   mount_uploader :image, PhotoUploader
   has_many :articles, dependent: :nullify
   validates :name, presence: true
+
+  def normalize_friendly_id(input)
+    "#{PinYin.of_string(input).to_s.to_slug.normalize.to_s}"
+  end
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 end
