5b2175bdeaf21f162de417bdb1239911486fde7b
suifeng <ganweiliang886@gmail.com>
2015-12-20 19:04:39 +0800

    fix in_groups_in error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_sites
   before_action :authorize
 
   private
+    def set_sites
+      @sites = Rails.cache.fetch("sites") do
+        Site.all.to_a
+      end
+    end
     def authorize
       if session[:admin]
         Rack::MiniProfiler.authorize_request
