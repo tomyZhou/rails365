f3a46ed72c82f8b2760aa88105defad07ed99b2b
suifeng <ganweiliang886@gmail.com>
2016-01-01 15:35:50 +0800

    最新文章改为更新时间来排序

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @articles = Rails.cache.fetch "articles" do
-      Article.except_body_with_default.order("id DESC").limit(10).to_a
+      Article.except_body_with_default.order("updated_at DESC").limit(10).to_a
     end
     @hot_articles = Rails.cache.fetch "hot_articles" do
       Article.except_body_with_default.select("visit_count").order("visit_count DESC").limit(10).to_a
