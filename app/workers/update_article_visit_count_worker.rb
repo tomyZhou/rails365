5653e23a0866cedee81c13524499d975f0740d69
suifeng <ganweiliang886@gmail.com>
2016-02-26 16:48:30 +0800

    fix update article visit count worker

--- app/workers/update_article_visit_count_worker.rb
+++ app/workers/update_article_visit_count_worker.rb
@@ -2,9 +2,13 @@ class UpdateArticleVisitCountWorker
   include Sidekiq::Worker
   def perform(article_id)
     logger.info 'update article visit count begin'
-    @article = Article.find(article_id)
-    @article.visit_count += 1
-    @article.save!(validate: false)
-    logger.info 'update article visit count end'
+    @article = Article.find_by(id: article_id)
+    if @article.present?
+      @article.visit_count += 1
+      @article.save!(validate: false)
+      logger.info 'article is not exists'
+    else
+      logger.info 'update article visit count end'
+    end
   end
 end
