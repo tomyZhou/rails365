5cf0df63fb6ea9b1fc2d68acd67d283085f9b459
suifeng <ganweiliang886@gmail.com>
2016-03-13 22:35:43 +0800

    fix username display

--- db/migrate/20160313075522_add_user_id_to_articles.rb
+++ db/migrate/20160313075522_add_user_id_to_articles.rb
@@ -2,14 +2,4 @@ class AddUserIdToArticles < ActiveRecord::Migration
   def change
     add_reference :articles, :user, index: true, foreign_key: true
   end
-
-  def data
-    user = User.create! email: "hfpp2012@aliyun.com", password: "12345678", login: "hfpp2012"
-    Article.update_all user_id: user.id
-  end
-
-  def rollback
-    User.find_by(email: "hfpp2012@aliyun.com").destroy
-    Article.update_all user_id: nil
-  end
 end
--- db/migrate/20160313134504_add_username_to_users.rb
+++ db/migrate/20160313134504_add_username_to_users.rb
@@ -3,4 +3,14 @@ class AddUsernameToUsers < ActiveRecord::Migration
     add_column :users, :username, :string
     add_index :users, :username, unique: true
   end
+
+  def data
+    user = User.create! email: "hfpp2012@aliyun.com", password: "12345678", username: "hfpp2012"
+    Article.update_all user_id: user.id
+  end
+
+  def rollback
+    User.find_by(email: "hfpp2012@aliyun.com").destroy
+    Article.update_all user_id: nil
+  end
 end
