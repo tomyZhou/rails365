a91126d46838cdb3144156ed3be20dbee3a9cc9e
suifeng <ganweiliang886@gmail.com>
2016-05-18 21:15:49 +0800

    fix puma config file

--- config/puma_app.rb
+++ config/puma_app.rb
@@ -18,10 +18,15 @@ stdout_redirect "#{app_path}/log/puma.log", "#{app_path}/log/puma.stderr.log", t
 # Set master PID and state locations
 pidfile "#{app_path}/pids/puma.pid"
 state_path "#{app_path}/tmp/sockets/puma.state"
-activate_control_app
+daemonize true
+preload_app!
 
 on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_path}/config/database.yml")[rails_env])
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
 end
