114cc249f22e9f55571bb2cf565a45fa050e577a
suifeng <ganweiliang886@gmail.com>
2016-05-27 15:14:12 +0800

    article model test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,19 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
 
-  let(:article) { FactoryGirl.create(:article) }
+  let(:article) { create(:article) }
 
-  subject { article }
+  describe "#title" do
+    context "正常" do
+      let(:article) { build(:article, title: "ttt") }
+      it { expect(article.valid?).to eq true }
+    end
 
-  it { should respond_to(:title) }
-
-  describe "标题不能为空" do
-    before { article.title = "" }
-    it { should_not be_valid }
+    context "为空" do
+      let(:article) { build(:article, title: "") }
+      it { expect(article.valid?).to eq false }
+    end
   end
 
-  describe "内容不能为空" do
-    before { article.body = "" }
-    it { should_not be_valid }
+  describe "#content" do
+    context "正常" do
+      let(:article) { build(:article, body: "cccc") }
+      it { expect(article.valid?).to eq true }
+    end
+
+    context "为空" do
+      let(:article) { build(:article, body: "") }
+      it { expect(article.valid?).to eq false }
+    end
+  end
+
+  describe "#user" do
+    it "should get user username" do
+      user = create(:user)
+      expect(create(:article, user: user).user.username).to eq user.username
+    end
+
+    it "should not be blank" do
+      article = build(:article, user: nil)
+      expect(article.valid?).to eq false
+    end
   end
 end
