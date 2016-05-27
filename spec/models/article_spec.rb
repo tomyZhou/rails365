27c47f6f8af52aa701eba87a17244d20e30d7fdc
suifeng <ganweiliang886@gmail.com>
2016-05-27 15:48:41 +0800

    fix article model test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -39,4 +39,30 @@ RSpec.describe Article, type: :model do
       expect(article.valid?).to eq false
     end
   end
+
+  describe "#group" do
+    it "should get group name" do
+      group = create(:group)
+      expect(create(:article, group: group).group.name).to eq group.name
+    end
+
+    it "should not be blank" do
+      article = build(:article, group: nil)
+      expect(article.valid?).to eq false
+    end
+
+    it "should get group count" do
+      group = create(:group)
+      article = create(:article, group: group)
+      expect(group.articles.count).to eq 1
+
+      article.destroy
+      expect(group.articles.count).to eq 0
+    end
+  end
+
+  it 'should covert body with Markdown on create' do
+    article = create(:article, body: '*foo*')
+    expect(MyMarkdown.render(article.body)).to eq("<p><em>foo</em></p>\n")
+  end
 end
