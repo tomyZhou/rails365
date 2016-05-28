33152ce3770f932158a80b670fb0f8b8e998ad96
suifeng <ganweiliang886@gmail.com>
2016-05-28 22:48:31 +0800

    fix migration

--- db/migrate/20160408060856_drop_homeland.rb
+++
@@ -1,7 +0,0 @@
-class DropHomeland < ActiveRecord::Migration
-  def change
-    drop_table :homeland_nodes
-    drop_table :homeland_topics
-    drop_table :homeland_replies
-  end
-end
--- db/migrate/20160523152434_remove_pg_search.rb
+++
@@ -1,9 +0,0 @@
-class RemovePgSearch < ActiveRecord::Migration
-  def up
-    execute("DROP INDEX articles_idx;")
-  end
-
-  def down
-    execute("CREATE INDEX articles_idx ON articles USING gin(to_tsvector('testzhcfg', title || ' ' || body));")
-  end
-end
