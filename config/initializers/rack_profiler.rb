ea108d6bb6bea9d6c0d6a617988ce0e2c7a96416
suifeng <ganweiliang886@gmail.com>
2016-05-29 10:51:39 +0800

    fix rack-mini-profiler

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,7 +1,9 @@
 require 'rack-mini-profiler'
-# initialization is skipped so trigger it
-Rack::MiniProfilerRails.initialize!(Rails.application)
 
-# set RedisStore
-# Rack::MiniProfiler.config.storage_options = { :host => '127.0.0.1', :port => 6379 }
-# Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+if !Rails.env.test?
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfiler.config.storage_options = { host: '127.0.0.1', port: '6379', db: 2 }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  Rack::MiniProfiler.config.disable_env_dump = true
+  Rack::MiniProfiler.config.skip_paths = ['/ws']
+end
