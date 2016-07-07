586defc2df6177379592b28ae22e5666d97eb084
suifeng <ganweiliang886@gmail.com>
2016-07-07 09:31:34 +0800

    add cap logs

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,3 +64,12 @@ namespace :deploy do
   task :restart do
   end
 end
+
+namespace :logs do
+  desc "tail rails logs" 
+  task :tail_rails do
+    on roles(:app) do
+      execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
+    end
+  end
+end
