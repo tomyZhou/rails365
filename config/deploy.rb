1cc77df73acba28b2d5545933cae3fa306c11993
yinsigan <ganweiliang886@gmail.com>
2017-05-01 23:55:18 +0800

    fix typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'rails365'
-set :repo_url, 'git@github.com:yinsigan/rails365.git'
+set :repo_url, 'ssh://git@gitlab.rails365.net:10022/hfpp2012/rails365.git'
 set :branch, 'master'
 set :deploy_to, "/home/hfpp2012/#{fetch(:application)}"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml')
