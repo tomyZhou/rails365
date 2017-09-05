4780f6b82f7f82bcf7d7cc0c0ecd6ce88188470e
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2017-09-05 17:31:15 +0800

    add google plus share

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook weibo qq douban wechat)
+  config.allow_sites = %w(twitter facebook weibo qq douban wechat google_plus)
 end
