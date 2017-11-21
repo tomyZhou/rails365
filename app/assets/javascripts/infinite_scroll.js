4fe99a70cafaf8dd18a63ed9939c373c0824e727
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2017-11-21 21:02:38 +0800

    infinite scroll

---
+++ app/assets/javascripts/infinite_scroll.js
@@ -0,0 +1,17 @@
+$(document).on('ready', function () {
+  var isLoading = false;
+  if ($('.my-pagination').size() > 0) {
+    $(window).on('scroll', function() {
+      var more_posts_url = $('.my-pagination a').attr('href');
+      if (!isLoading && more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
+        isLoading = true;
+        $('.pagination-text').html("正在加载下一页...");
+        $.getScript(more_posts_url).done(function (data,textStatus,jqxhr) {
+          isLoading = false;
+        }).fail(function() {
+          isLoading = false;
+        });
+      }
+    });
+  }
+});
