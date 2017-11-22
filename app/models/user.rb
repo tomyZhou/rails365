f095f7623c287e57a8b04156594ea7ba83d06aa1
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2017-11-22 14:54:14 +0800

    fix user nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = "from_github_#{auth.info.email}"
-      user.username = Devise.friendly_token[0, 20]
+      user.username = auth.nickname
       user.password = Devise.friendly_token[0, 20]
     end
   end
