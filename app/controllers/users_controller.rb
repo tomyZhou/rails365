59c2409a68292549740083f0a04f43d9d7b70239
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2017-12-08 17:22:51 +0800

    change like

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def common
     @user = User.find(params[:id])
-    @favourite_articles = @user.like_articles
+    @favourite_articles = @user.like_articles.page(params[:page])
     @favourite_movies = @user.like_movies
     @favourite_softs = @user.like_softs
     @articles = @user.articles.order('id DESC').page(params[:page])
