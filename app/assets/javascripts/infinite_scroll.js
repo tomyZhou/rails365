aca8e6c2a28f97e8f5847b2799ce596130d6c4c4
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-04-14 21:55:21 +0800

    fix typo

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -3,7 +3,7 @@ $(document).on('ready', function () {
   if ($('.my-pagination').size() > 0) {
     $(window).on('scroll', function() {
       var more_posts_url = $('.my-pagination a').attr('href');
-      if (!isLoading && more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 682) {
+      if (!isLoading && more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 2282) {
         isLoading = true;
         $('.pagination-text').html("正在加载下一页 <img src='/ajax-loader.gif' />");
         $.getScript(more_posts_url).done(function (data,textStatus,jqxhr) {
