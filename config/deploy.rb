45b33f1ae1ee9b4312635d694658505b6e431e63
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-04-19 02:58:20 +0800

    remove rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,15 @@ set :deploy_to, "/home/deploy/#{fetch(:application)}"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/settings.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'pids', 'tmp/sockets', 'public/system')
 
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.3.0'
+# set :rbenv_type, :user # or :system, depends on your rbenv setup
+# set :rbenv_ruby, '2.5.1'
 
 # in case you want to set ruby version from the file:
 # set :rbenv_ruby, File.read('.ruby-version').strip
 #
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w( rake gem bundle ruby rails sidekiq sidekiqctl )
-set :rbenv_roles, :all # default value
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w( rake gem bundle ruby rails sidekiq sidekiqctl )
+# set :rbenv_roles, :all # default value
 
 set :bundle_flags, ''
 
